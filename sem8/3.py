875172ab2ed1071006d4ddfb408be669e65730f8
rost <poshivanik@gmail.com>
2022-12-17 23:12:55 +0500

    пересдача дз 17/12/2022

--- sem8/3.py
+++ sem8/3.py
@@ -3,15 +3,19 @@ class DivNullEror:
         self.div = div
         self.den = den
 
-
-    def divide_by_null(div, den):
+    def divide_by_null(div=int, den=int):
         try:
-            return (div / den)
-        except:
-            return (f"∞")
+            if den == 0:
+                raise ZeroDivisionError(f'Деление на ноль недопустимо')
+        except ZeroDivisionError:
+            return (f"Деление на ноль")
+        except OwnError as err:
+            print(err)
+        else:
+            return f'Всё хорошо {div}/{den} = {div / den}'
 
 
-
+div = DivNullEror(100, 0)
 print(DivNullEror.divide_by_null(10, 0))
 print(DivNullEror.divide_by_null(10, 0.1))
 print(DivNullEror.divide_by_null(100, 0))
